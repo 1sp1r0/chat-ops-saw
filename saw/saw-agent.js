7ee6bc10f51fa1a27362eb4882c7e823fd107fa0
huayongj <yong-jianh@hpe.com>
2016-04-07 16:36:56 +0800

    Update saw-agent model

--- saw/saw-agent.js
+++ saw/saw-agent.js
@@ -9,9 +9,10 @@ function SAW() {
 	this.client = new Client();
 	this.sawUrl = '';
 	this.tenantId = '';
-
+	this.baseUrlEntities = {
+		Incident: ''
+	};
 	this.baseUrlBulk = '';
-	this.baseUrlEntity = '';
 	this.baseUrlPerson = '';
 	this.baseUrlPersonGroup = '';
 	this.baseUrlITProcessRecordCategory = '';
@@ -25,49 +26,6 @@ SAW.prototype.__httpGet = function (path, callback) {
 
 SAW.prototype.__httpPost = function (path, data, callback) {
 	this.client.post(this.sawUrl + path, { headers: this.headers, data: data }, callback);
-};
-
-SAW.prototype.__getEntity = function (entityId) {
-	var that = this;
-	var layout = ['Id','DisplayLabel','AssignedGroup.Id','AssignedGroup.Name'];
-	return Q.Promise(function (resolve, reject, notify) {
-		that.__httpGet(that.baseUrlEntity + util.format('/%s?layout=%s', entityId, layout.join()), function (data, res) {
-			if (res.statusCode == 200) {
-				if (data.entities.length > 0) {
-					resolve(data.entities[0]);
-				} else {
-					reject('Cannot find entity: ' + entityId);
-				}
-			} else {
-				reject(res.statusMessage);
-			}
-		});
-	});
-};
-
-SAW.prototype.__getPersonsByGroup = function (entity) {
-	entity['persons'] = [];
-	var that = this;
-	var layout = ['Id','Name','Email'];
-	return Q.Promise(function (resolve, reject, notify) {
-		var assignedGroup = entity.related_properties.AssignedGroup;
-		if (assignedGroup === undefined) {
-			resolve(entity);
-		} else {
-			that.__httpGet(that.baseUrlPerson + util.format('?filter=(PersonToGroup[Id = %s])&layout=%s', assignedGroup.Id, layout.join()), function (data, res) {
-				if (res.statusCode == 200) {
-					if (data.entities.length > 0) {
-						entity['persons'] = data.entities;
-					} else {
-						console.log('No persons in group ' + assignedGroup.Name);
-					}
-				} else {
-					console.log(res.statusMessage);
-				}
-				resolve(entity);
-			});
-		}
-	});
 };
 
 SAW.prototype.__createUpdateOperation = function (entityType, entityId, properties) {
@@ -85,7 +43,6 @@ SAW.prototype.__createUpdateOperation = function (entityType, entityId, properti
 SAW.prototype.__executeUpdateOperation = function (postBody) {
 	var that = this;
 	return Q.Promise(function (resolve, reject, notify) {
-		console.log(JSON.stringify(postBody));
 		that.__httpPost(that.baseUrlBulk, postBody, function (data, res) {
 			if (res.statusCode == 200) {
 				// data.meta.completion_status === 'OK'
@@ -97,7 +54,43 @@ SAW.prototype.__executeUpdateOperation = function (postBody) {
 	});
 };
 
-SAW.prototype.__getPersonByEmail = function (email) {
+SAW.prototype.getEntity = function (entityType, entityId) {
+	var that = this;
+	var layout = ['Id','DisplayLabel'];
+	return Q.Promise(function (resolve, reject, notify) {
+		that.__httpGet(that.baseUrlEntities[entityType] + util.format('/%s?layout=%s', entityId, layout.join()), function (data, res) {
+			if (res.statusCode == 200) {
+				if (data.entities.length > 0) {
+					resolve(data.entities);
+				} else {
+					reject('Cannot find entity: ' + entityId);
+				}
+			} else {
+				reject(res.statusMessage);
+			}
+		});
+	});
+};
+
+SAW.prototype.getPersonsByGroupId = function (groupId) {
+	var that = this;
+	var layout = ['Id','Name','Email'];
+	return Q.Promise(function (resolve, reject, notify) {
+		that.__httpGet(that.baseUrlPerson + util.format('?filter=(PersonToGroup[Id = %s])&layout=%s', groupId, layout.join()), function (data, res) {
+			if (res.statusCode == 200) {
+				if (data.entities.length > 0) {
+					resolve(data.entities);
+				} else {
+					reject('No person in group: ' + groupId);
+				}
+			} else {
+				reject(res.statusMessage);
+			}
+		});
+	});
+};
+
+SAW.prototype.getPersonByEmail = function (email) {
 	var that = this;
 	var layout = ['Id','Name','Email'];
 	var url = that.baseUrlPerson + util.format("?filter=((IsSystemIntegration != 'true' and IsSystem != 'true') and Email = ('%s'))&layout=%s", email, layout.join());
@@ -105,7 +98,7 @@ SAW.prototype.__getPersonByEmail = function (email) {
 		that.__httpGet(url, function (data, res) {
 			if (res.statusCode === 200) {
 				if (data.entities.length > 0) {
-					resolve(data.entities[0]);
+					resolve(data.entities);
 				} else {
 					reject('Cannot find person by email: ' + email);
 				}
@@ -116,7 +109,7 @@ SAW.prototype.__getPersonByEmail = function (email) {
 	});
 };
 
-SAW.prototype.__getGroupByName = function (name) {
+SAW.prototype.getGroupByName = function (name) {
 	var that = this;
 	var layout = ['Id', 'Name', 'GroupType'];
 	var url = that.baseUrlPersonGroup + util.format("?filter=Name = ('%s')&layout=%s", name, layout.join());
@@ -124,7 +117,7 @@ SAW.prototype.__getGroupByName = function (name) {
 		that.__httpGet(url, function (data, res) {
 			if (res.statusCode === 200) {
 				if (data.entities.length > 0) {
-					resolve(data.entities[0]);
+					resolve(data.entities);
 				} else {
 					reject('Cannot find group by name: ' + name);
 				}
@@ -135,16 +128,55 @@ SAW.prototype.__getGroupByName = function (name) {
 	});
 };
 
-SAW.prototype.__getCategoryByName = function (name) {
+SAW.prototype.updateField = function (entityType, entityId, properties) {
+	return this.__executeUpdateOperation(this.__createUpdateOperation(entityType, entityId, properties));
+};
+
+SAW.prototype.assignPersonByEmail = function(entityType, entityId, field, email) {
+	var that = this;
+	return this.getPersonByEmail(email).then(function (persons) {
+		return Q.Promise(function (resolve, reject, notify) {
+			if (persons.length === 1) {
+				resolve(persons[0]);
+			} else {
+				reject(util.format('%d persons are found', persons.length));
+			}
+		}).then(function (person) {
+			var personId = person.properties.Id;
+			var property = {};
+			property[field] = personId;
+			return  that.updateField(entityType, entityId, property);
+		});
+	});
+};
+
+SAW.prototype.assignGroupByGroupName = function(entityType, entityId, field, groupName) {
+	var that = this;
+	return this.getGroupByName(groupName).then(function (groups) {
+		return Q.Promise(function (resolve, reject, notify) {
+			if (groups.length === 1) {
+				resolve(groups[0]);
+			} else {
+				reject(util.format('%d groups are found', groups.length));
+			}
+		}).then(function (group) {
+			var groupId = group.properties.Id;
+			var property = {};
+			property[field] = groupId;
+			return  that.updateField(entityType, entityId, property);
+		});
+	});
+};
+
+SAW.prototype.getCategoryByName = function (name) {
 	var that = this;
 	var layout = ['Id', 'DisplayLabel'];
 	var url = that.baseUrlITProcessRecordCategory + util.format("?filter=(IsActive = 'true' and (DisplayLabel startswith ('%s') or Level1Parent startswith ('%s') or Level2Parent startswith ('%s')))&layout=%s", name, name, name, layout.join());
-	console.log(url);
 	return Q.Promise(function (resolve, reject, notify) {
 		that.__httpGet(url, function (data, res) {
 			if (res.statusCode === 200) {
 				if (data.entities.length > 0) {
-					resolve(data.entities[0]);
+					resolve(data.entities);
 				} else {
 					reject('Cannot find category by name: ' + name);
 				}
@@ -155,35 +187,13 @@ SAW.prototype.__getCategoryByName = function (name) {
 	});
 };
 
-// ----------------------------------------------------------------------------------------------------------------------------------
-
-SAW.prototype.assignPerson = function(entityType, entityId, field, email) {
-	var that = this;
-	return this.__getPersonByEmail(email).then(function (person) {
-		var personId = person.properties.Id;
-		var body = {};
-		body[field] = personId;
-		return that.__executeUpdateOperation(that.__createUpdateOperation(entityType, entityId, body));
-	});
-};
-
-SAW.prototype.assignGroup = function(entityType, entityId, field, groupName) {
-	var that = this;
-	return this.__getGroupByName(groupName).then(function (group) {
-		var groupId = group.properties.Id;
-		var body = {};
-		body[field] = groupId;
-		return that.__executeUpdateOperation(that.__createUpdateOperation(entityType, entityId, body));
-	});
-};
-
 SAW.prototype.assignCategory = function (entityType, entityId, categoryName) {
 	var that = this;
 	return this.__getCategoryByName(categoryName).then(function (category) {
 		var categoryId = category.properties.Id;
-		var body = {};
-		body['Category'] = categoryId;
-		return that.__executeUpdateOperation(that.__createUpdateOperation(entityType, entityId, body));
+		var property = {};
+		property['Category'] = categoryId;
+		return that.updateField(entityType, entityId, property);
 	});
 };
 
@@ -191,11 +201,16 @@ SAW.prototype.login = function (url, tenantId, username, password) {
 	var that = this;
 	that.sawUrl = url;
 	that.tenantId = tenantId;
-	that.baseUrlEntity = util.format('/rest/%s/ems/Incident', that.tenantId);
+	that.baseUrlBulk = util.format('/rest/%s/ems/bulk', that.tenantId);
+
+	_.each(that.baseUrlEntities, function (v, k, o) {
+		o[k] = util.format('/rest/%s/ems/%s', that.tenantId, k);
+	});
+
 	that.baseUrlPerson = util.format('/rest/%s/ems/Person', that.tenantId);
 	that.baseUrlPersonGroup = util.format('/rest/%s/ems/PersonGroup', that.tenantId);
 	that.baseUrlITProcessRecordCategory = util.format('/rest/%s/ems/ITProcessRecordCategory', that.tenantId);
-	that.baseUrlBulk = util.format('/rest/%s/ems/bulk', that.tenantId);
+	
 
 	that.__httpPost('/auth/authentication-endpoint/authenticate/login?TENANTID=' + tenantId, { 'Login': username, 'Password': password }, function (data, res) {
 		if (res.statusCode == 200) {
@@ -207,13 +222,9 @@ SAW.prototype.login = function (url, tenantId, username, password) {
 	});
 };
 
-SAW.prototype.showDetail = function (entityId) {
-	return this.__getEntity.bind(this)(entityId).then(this.__getPersonsByGroup.bind(this));
-};
-
-SAW.prototype.closeIncident = function(entityId) {
-
-};
+// SAW.prototype.showDetail = function (entityId) {
+// 	return this.getEntity.bind(this)(entityId).then(this.__getPersonsByGroup.bind(this));
+// };
 
 SAW.prototype.onAuthorized = function (callback) {
 	this.eventEmitter.on(this.EVENT_SAW_AUTHORIZED, callback);
