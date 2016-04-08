36bb68c30658a9f943c299b67ea7e57df5713246
Shen Yu <yu.shen@hp.com>
2016-04-08 14:25:54 +0800

    FIX, findByName is undefied

--- saw/model.js
+++ saw/model.js
@@ -9,7 +9,7 @@ model.type('SawAgent',{
 	assignGroupByGroupName: Function,
 	assignPersonByEmail: Function,
 	login: Function,
-	findFieldByName, Function
+	findFieldByName: Function
 });
 
 model.type('SlackAgent',{
