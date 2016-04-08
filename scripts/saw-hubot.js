be0760290077f8c7a0ca206e3e7116a0d4927ab2
Shen Yu <yu.shen@hp.com>
2016-04-08 13:18:28 +0800

    US, bot sopport update move phase

--- scripts/saw-hubot.js
+++ scripts/saw-hubot.js
@@ -28,32 +28,12 @@ function parsePersons(personsStr) {
 	return (personsStr||'').slice(1,personsStr.length - 1).split(', ');
 }
 
-sawAgent.login('http://ppmqavm155.asiapacific.hpqcorp.net:8000/','100000002','devUser2@hp.com','Password1');
+sawAgent.login('http://sheyu4.hpeswlab.net:8000/','100000002','devUser2@hp.com','Password1');
 
 
 module.exports = function(hubot) {
-	hubot.hear(/badger/i,function(res) {
-		res.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
-	});
-
-	hubot.respond(/Update #(\d*), (.*) assignee assign to @(.*)/i, function(res) {
-		var incidentId = res.match[1], 
-			fields = res.match[2].split(','),
-			assignee = slackAgent.findUserByName(res.match[3]), 
-			entityInfo = parseRoomName(res.message.room);
-		if(!assignee) {
-			res.send('who is ' + res.match[3] + '?');
-			return;
-		}
-
-		res.send('OK, I will update ' + incidentId);
-
-		Q.all(fields.map(function(field) {
-			return sawAgent.assignPerson('Incident',entityInfo.Id,field,assignee.email);
-		}));
-	});
-
-	hubot.router.put('/saw/:entityType/:id', function(req, res) {
+	//create room
+	hubot.router.post('/saw/:entityType/:id', function(req, res) {
 		console.log(req.body);
 		var id = req.params.id,
 			entityType = req.params.entityType,
@@ -83,12 +63,66 @@ module.exports = function(hubot) {
 		res.send('OK');
 	});
 
-	hubot.router.post('/saw/:entityType/:id', function(req, res) {
+	//update message
+	hubot.router.put('/saw/:entityType/:id', function(req, res) {
 		var id = req.params.id,
 			entityType = req.params.entityType,
 			message = req.body.message;
-		console.log('receive saw entity updated message',id, message);			
+		console.log('receive saw entity updated message',id, message);
+
+		//entity check
 		slackAgent.sendMessage(genRoomName(entityType,id), message, HUBUT_NAME);
 		res.send('OK');
 	});
+
+	//message assign incident, example: @saw Update Incident #123456, Owner, expert assign to @yu
+
+	hubot.respond(/(Update (.*) #(\d*),)? (.*) assign to @(.*)/i, function(res) {
+		var entityType = res.match[1];
+		var id = res.match[2];
+		var fieldNames = res.match[3].split(',');
+		var assignee = slackAgent.findUserByName(res.match[4]);
+		if(!assignee) {
+			res.send('Error');
+			return;
+		}
+
+		Q.all(fieldNames.map(function(fieldName) {
+			var field = sawAgent.findFieldByName(entityType, fieldName.trim());
+			sawAgent.assignPersonByEmail(entityType,id, field,assignee.email);
+		})).fail(function(message) {
+			res.send(message);
+		});
+	});
+
+	//message assign group, example: @saw Update Incident #123456, Expert Group assign group to CBA Group
+	
+	hubot.respond(/Update (.*) #(\d*), (.*) assign group to (.*)/i, function(res) {
+		var entityType = res.match[1];
+		var id = res.match[2];
+		var fieldNames = res.match[3].split(',');
+		var groupName = res.match[4];
+
+		Q.all(fieldNames.map(function(fieldName) {
+			var field = sawAgent.findFieldByName(entityType, fieldName.trim());
+			sawAgent.assignGroupByGroupName(entityType,id, field.trim(),groupName);
+		})).fail(function(message) {
+			res.send(message);
+		});
+	});
+
+
+	//message move phase, example: @saw Update Incident #2182931,move phase to abc
+	hubot.respond(/Update (.*) #(\d*), move phase to (.*)/i, function(res) {
+		var entityType = res.match[1];
+		var id = res.match[2];
+		var phase = res.match[3];
+
+		sawAgent.updateField(entityType,id, {PhaseId:phase}).then(function() {
+			res.send('done');
+		}).fail(function(message) {
+			res.send(message);
+		});
+	});
+
 }
