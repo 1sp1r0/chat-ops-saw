0ec62ef4d10cbe0c9b76107da383aae803032da6
Shen Yu <yu.shen@hp.com>
2016-04-08 19:02:01 +0800

    US, add CLI

--- scripts/saw-hubot.js
+++ scripts/saw-hubot.js
@@ -17,6 +17,12 @@ function genRoomName(entityType, id) {
 }
 
 function parseRoomName(roomName) {
+	if(roomName === 'Shell') {
+		return {
+			entityType: 'Incident',
+			Id: 123456
+		}
+	}
 	return roomEntitiesMapping[roomName];
 }
 
@@ -75,13 +81,19 @@ module.exports = function(hubot) {
 		res.send('OK');
 	});
 
-	//message assign incident, example: @saw Update Incident #123456, Owner, expert assign to @yu
 
-	hubot.respond(/(Update (.*) #(\d*),)? (.*) assign to @(.*)/i, function(res) {
-		var entityType = res.match[1];
-		var id = res.match[2];
-		var fieldNames = res.match[3].split(',');
-		var assignee = slackAgent.findUserByName(res.match[4]);
+	//assign person, example: @saw assign-person Incident #1234 Owner to @yu
+	hubot.respond(/assign-person( (\w+) #(\d+))? ([\w, ]+) to @([\w ]+)/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var id = res.match[3] || entityInfo.Id;
+		var fieldNames = res.match[4].split(',');
+		var assigneeName = res.match[5];
+
+		console.log('assign-person',entityType,id, fieldNames, assignee);
+
+		var assignee = slackAgent.findUserByName(assigneeName);
+
 		if(!assignee) {
 			res.send('Error');
 			return;
@@ -95,13 +107,19 @@ module.exports = function(hubot) {
 		});
 	});
 
-	//message assign group, example: @saw Update Incident #123456, Expert Group assign group to CBA Group
-	
-	hubot.respond(/Update (.*) #(\d*), (.*) assign group to (.*)/i, function(res) {
-		var entityType = res.match[1];
-		var id = res.match[2];
-		var fieldNames = res.match[3].split(',');
-		var groupName = res.match[4];
+	hubot.respond(/help/i, function(res){
+		res.send('help...');
+	});
+
+	//assign group, example: @saw assign-group Incident #1234 Owner to yu
+	hubot.respond(/assign-group( (\w+) #(\d+))? ([\w, ]+) to ([\w ]+)/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var id = res.match[3] || entityInfo.Id;
+		var fieldNames = res.match[4].split(',');
+		var groupName = res.match[5];
+
+		console.log('assign-group',entityType,id, fieldNames, groupName);
 
 		Q.all(fieldNames.map(function(fieldName) {
 			var field = sawAgent.findFieldByName(entityType, fieldName.trim());
@@ -112,11 +130,14 @@ module.exports = function(hubot) {
 	});
 
 
-	//message move phase, example: @saw Update Incident #2182931,move phase to abc
-	hubot.respond(/Update (.*) #(\d*), move phase to (.*)/i, function(res) {
-		var entityType = res.match[1];
-		var id = res.match[2];
-		var phase = res.match[3];
+	//move phase, example: @saw move-phase Incident #1234 to Close
+	hubot.respond(/move-phase( (\w+) #(\d+))? to ([\w]+)/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var id = res.match[3] || entityInfo.Id;
+		var phase = res.match[4];
+
+		console.log('move-phase',entityType,id, phase);
 
 		sawAgent.updateField(entityType,id, {PhaseId:phase}).then(function() {
 			res.send('done');
@@ -125,4 +146,44 @@ module.exports = function(hubot) {
 		});
 	});
 
+	//get detail, example: @saw show-detail Incident #1234
+	hubot.respond(/show-detail( (\w+) #(\d+))?/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var id = res.match[3] || entityInfo.Id;
+
+		console.log('show-detail',entityType,id );
+
+	});
+
+	//get attributes, example: @saw show Change #12345 Owner,Test
+	hubot.respond(/show( (\w+) #(\d+))?([\w, ]*)/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var id = res.match[3] || entityInfo.Id;
+		var fieldNames = res.match[4].split(',');
+
+		console.log('show',entityType, id, fieldNames);
+
+	});
+
+	//set attribute, example: @saw update Change #12345 Owner,Test value XXX
+	hubot.respond(/update( (\w+) #(\d+))? ([\w, ]+) value ([\w ]+)/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var id = res.match[3] || entityInfo.Id;
+		var fieldNames = res.match[4].split(',');
+		var value = res.match[5];
+
+		console.log('update',entityType, id, fieldNames, value);
+	});
+
+	//search, example: @saw search Change by abcd XXX
+	hubot.respond(/search( (\w+))? by ([\w, ]+)/i, function(res) {
+		var entityInfo = parseRoomName(res.message.room);
+		var entityType = res.match[2] || entityInfo.entityType; 
+		var keywords = res.match[3].split(',').join(' ');
+
+		console.log('search',entityType, keywords);
+	});
 }
